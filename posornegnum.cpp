43b4e620f25aec86442e966065b9ba269924d2a8
Prashant Rathore <pp454460@gmail.com>
2023-06-11 16:46:25 +0530

    init

---
+++ posornegnum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=96;
+    if (x>0){
+        cout<<"This number is positive";
+    }
+    else if(x<0){
+        cout<<"This number is negative";
+    }
+    
+    else{
+
+        cout<<"ZERO"<<endl;
+    }
+    return 0;
+    
+    
+}
